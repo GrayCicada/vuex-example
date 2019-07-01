b5341b1c1f7e34de264ed2e68dc16b2310e8188b
GreenCicida <clamorruhe@gmail.com>
2019-07-01 11:57:07 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+   <PostForm />
+   <hr>
+   <h4>Posts count: {{postsCount}}</h4>
+   <div class="post" v-for="post in validPosts" :key="post.id">
+      <h3>{{post.title}}</h3>
+      <p>{{post.body}}</p>
+   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+   import {mapGetters, mapActions} from 'vuex'
+   import PostForm from './components/PostForm'
+   export default {
+      name: 'app',
+      // computed: {
+      //    allPosts(){
+      //       return this.$store.getters.allPosts;
+      //    }
+      // },
+      // Альтернативный способ
+      computed: mapGetters(['validPosts', 'postsCount']),
+      methods: mapActions(['fetchPosts']),
+      async mounted() {
+         // this.$store.dispatch('fetchPosts')
+         this.fetchPosts(15)
+      },
+      components: {
+         PostForm
+      }
+   }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin: 60px auto;
+   width: 400px;
+}
+.post{
+   text-align: left;
+   border: 1px solid #eee;
+   border-radius: 5px;
+   margin-bottom: 1rem;
+   padding: 1rem;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <form>
+     <input type="text" placeholder="title" v-model="title">
+     <input type="text" placeholder="body" v-model="body">
+     <button type="submit" @click.prevent="submit">Create post</button>
+  </form>
+</template>
+
+<script>
+import {mapMutations} from 'vuex'
+export default {
+   data(){
+      return{
+         title: '',
+         body: ''
+      }
+   },
+   methods: {
+      ...mapMutations(['createPost']),
+      submit(){
+         this.createPost({
+            title: this.title,
+            body: this.body,
+            id: Date.now()
+         });
+         this.title = this.body = ''
+      }
+   }
+}
+</script>
+
+<style lang="css" scoped>
+input{
+   display: block;
+   width: 100%;
+   border: 1px solid #ddd;
+   border-radius: 4px;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+button{
+   background: green;
+   color: #fff;
+   border-radius: 4px;
+   padding: 0.5rem 0.75rem;
+   border: 1px solid green;
+   font-size: 1rem;
+}
+</style>
